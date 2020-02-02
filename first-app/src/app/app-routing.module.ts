c9820391b241470d7a443cdaebf3fee69ae17ba8
Chinna Atchuta Rakesh Nalla <chinnaatchutarakeshnalla@Chinnas-MacBook-Pro-8.local>
2020-02-02 13:26:20 -0700

    adding files to first-app

---
+++ first-app/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './Third-component/third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
+import { SixthComponentComponent } from './sixth-component/sixth-component.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: 'home', component:HomeComponent},
+  { path: 'first-component', component:FirstComponentComponent },
+  { path: 'Divya', component:SecondComponentComponent},
+  { path: 'Third', component:ThirdComponentComponent},
+  { path: 'fourth', component:FourthComponentComponent},
+  { path: 'sixth', component:SixthComponentComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ first-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './Third-component/third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
+import { SixthComponentComponent } from './sixth-component/sixth-component.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    FourthComponentComponent,
+    SixthComponentComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
